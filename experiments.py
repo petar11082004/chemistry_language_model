858bce2eb69a45efb9fc037159567163667535f0
Petar Nikolaev Panayotov <pp583@cam.ac.uk>
2025-08-13 18:34:27 +0100

    added stretched and bended molecules to database

--- experiments.py
+++ experiments.py
@@ -3,12 +3,17 @@ from rdkit.Chem import rdMolTransforms as T
 from rdkit.Chem import rdDetermineBonds
 import numpy as np
 
-xyz = """4
-ammonia
-N	0.0000	0.0000	0.0000
-H	0.0000	-0.9377	-0.3816
-H	0.8121	0.4689	-0.3816
-H	-0.8121	0.4689	-0.3816
+xyz = """9
+ethanol
+C	1.1879	-0.3829	0.0000
+C	0.0000	0.5526	0.0000
+O	-1.1867	-0.2472	0.0000
+H	-1.9237	0.3850	0.0000
+H	2.0985	0.2306	0.0000
+H	1.1184	-1.0093	0.8869
+H	1.1184	-1.0093	-0.8869
+H	-0.0227	1.1812	0.8852
+H	-0.0227	1.1812	-0.8852
 """
 
 mol = Chem.MolFromXYZBlock(xyz)
@@ -21,19 +26,17 @@ Chem.GetSymmSSSR(mol)   # initializes RingInfo; no-op if no rings
 conf = mol.GetConformer()
 
 # Now transforms work without errors:
-'''
-i, j = 0, 1  
+
+i, j = 1, 0 
 d = T.GetBondLength(conf, i, j)
 T.SetBondLength(conf, i, j, d * 1.1)  # stretch +10%
-'''
-i, j, k = 1, 0, 2      
+"""
+i, j, k = 2, 0, 1     
 ang = T.GetAngleDeg(conf, i, j, k)
-T.SetAngleDeg(conf, i, j, k, ang * 0.9)
-
+T.SetAngleDeg(conf, i, j, k, ang * 1.1)
+"""
 # Extract coordinates back out
 pts = conf.GetPositions()
 geom = "\n".join(f"{a.GetSymbol()}\t{xyz[0]:.4f}\t{xyz[1]:.4f}\t{xyz[2]:.4f}"
                  for a, xyz in zip(mol.GetAtoms(), pts))
 print(geom)
-
-
