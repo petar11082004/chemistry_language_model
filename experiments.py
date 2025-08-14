9f974af8b83c39b3daf26c59cc8169337a53b48f
Petar Nikolaev Panayotov <pp583@cam.ac.uk>
2025-08-14 10:00:13 +0100

    now we have a complete set with stretched and bended molecules

--- experiments.py
+++ experiments.py
@@ -3,17 +3,15 @@ from rdkit.Chem import rdMolTransforms as T
 from rdkit.Chem import rdDetermineBonds
 import numpy as np
 
-xyz = """9
-ethanol
-C	1.1879	-0.3829	0.0000
-C	0.0000	0.5526	0.0000
-O	-1.1867	-0.2472	0.0000
-H	-1.9237	0.3850	0.0000
-H	2.0985	0.2306	0.0000
-H	1.1184	-1.0093	0.8869
-H	1.1184	-1.0093	-0.8869
-H	-0.0227	1.1812	0.8852
-H	-0.0227	1.1812	-0.8852
+xyz = """7
+methylamine
+C	0.0583	0.7129	0.0000	 	
+N	0.0583	-0.7726	0.0000	 	
+H	-0.9425	1.1529	0.0000	 	
+H	0.5877	1.0695	0.8821	 	
+H	0.5877	1.0695	-0.8821	 	
+H	-0.4953	-1.0804	-0.8165	 	
+H	-0.4953	-1.0804	0.8165
 """
 
 mol = Chem.MolFromXYZBlock(xyz)
@@ -26,15 +24,15 @@ Chem.GetSymmSSSR(mol)   # initializes RingInfo; no-op if no rings
 conf = mol.GetConformer()
 
 # Now transforms work without errors:
-
-i, j = 1, 0 
-d = T.GetBondLength(conf, i, j)
-T.SetBondLength(conf, i, j, d * 1.1)  # stretch +10%
 """
-i, j, k = 2, 0, 1     
+i, j = 1,5 
+d = T.GetBondLength(conf, i, j)
+T.SetBondLength(conf, i, j, d * 0.9)  # stretch +10%
+"""
+i, j, k = 6, 1, 5     
 ang = T.GetAngleDeg(conf, i, j, k)
 T.SetAngleDeg(conf, i, j, k, ang * 1.1)
-"""
+
 # Extract coordinates back out
 pts = conf.GetPositions()
 geom = "\n".join(f"{a.GetSymbol()}\t{xyz[0]:.4f}\t{xyz[1]:.4f}\t{xyz[2]:.4f}"
